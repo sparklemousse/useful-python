9df5188211517944d179ed23864ba28a5429d5a3
sparklemousse <paul.cronk@gmail.com>
2018-01-05 17:00:19 +0000

    Create 404-checking.py

---
+++ 404-checking.py
@@ -0,0 +1,34 @@
+import requests
+#count = 1
+
+def get_status_code(url):
+    try:
+        r = requests.get(url)
+        print ("Processing - " + url)
+
+        if len(r.history) > 0:
+            chain = ""
+            code = r.history[0].status_code
+            final_url = r.url
+            for resp in r.history:
+                chain += resp.url + " | "
+            return str(code) + '\t' + str(len(r.history)) + '\t' + chain + '\t' + final_url + '\t'
+        else:
+            return str(r.status_code) + '\t\t\t\t'
+    except requests.ConnectionError:
+        print("Error: failed to connect to " + url)
+        return '0\t\t\t\t'
+
+    #count = count + 1
+
+input_file = 'urls-list.txt'
+output_file = 'output.txt'
+
+with open(output_file, 'w') as o_file:
+    o_file.write('URL\tStatus\tNumberredirects\tRedirect Chain\tFinal URL\t\n')
+    f = open(input_file, "r")
+    lines = f.read().splitlines()
+    for line in lines:
+        code = get_status_code(line)
+        o_file.write(line + "\t" + str(code) + "\t\n")
+    f.close()
